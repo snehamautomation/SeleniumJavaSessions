067327540d8fed18c212a3ded34f320d24f77224
snehamautomation <snehaminachi@gmail.com>
2023-08-17 15:03:11 +0530

    first commit

---
+++ FirstJavaProject/src/FirstProgram/FirstJavaProgram.java
@@ -0,0 +1,15 @@
+package FirstProgram;
+
+public class FirstJavaProgram {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello");
+
+		
+        
+	}
+
+}
